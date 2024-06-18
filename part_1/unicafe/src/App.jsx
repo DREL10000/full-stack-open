7d69234105389762c0ea3531287d93a48e6e65af
DREL10000 <variabledev1@gmail.com>
2024-06-18 14:53:26 +0200

    Complete unicafe part1 task

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -6,14 +6,41 @@ const Button = ({onClick, text}) => {
   )
 }
 
-const Stats = ({good, neutral, bad}) =>{
-  return (
-    <div>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-    </div>
+const StatisticLine = ({text, value}) =>{
+  return(
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
+
+}
+
+const Statistics = ({good, neutral, bad, total}) =>{
+    if (total > 0)
+    {
+      return (
+        <>
+          <h1>statistics</h1>
+          <table>
+            <tbody>
+            <StatisticLine text="good" value={good}/>
+            <StatisticLine text="neutral" value={neutral}/>
+            <StatisticLine text="bad" value={bad}/>
+            <StatisticLine text="all" value={total}/>
+            <StatisticLine text="average" value={((good * 1) + (neutral * 0) + (bad * -1)) / total}/>
+            <StatisticLine text="positive" value={`${(good / total) * 100} %`}/>
+            </tbody>
+          </table>
+        </>
+      )
+    }
+    return (
+      <>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </>
+    )
 }
 
 const App = () => {
@@ -40,8 +67,7 @@ const App = () => {
       <Button onClick={handleNeutral} text="neutral"/>
       <Button onClick={handleBad} text="bad"/>
       
-      <h1>statistics</h1>
-      <Stats good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} total={good + neutral + bad}/>
 
     </div>
   )
