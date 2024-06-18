c9252fe06b12d75b6791af634d67a064536c90c7
DREL10000 <variabledev1@gmail.com>
2024-06-18 12:45:32 +0200

    Complete part 1 task 1.6

---
+++ part_1/unicafe/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+const Button = ({onClick, text}) => {
+  return(
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const Stats = ({good, neutral, bad}) =>{
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+      setGood(good + 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+}
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={handleGood} text="good"/>
+      <Button onClick={handleNeutral} text="neutral"/>
+      <Button onClick={handleBad} text="bad"/>
+      
+      <h1>statistics</h1>
+      <Stats good={good} neutral={neutral} bad={bad}/>
+
+    </div>
+  )
+}
+
+export default App
